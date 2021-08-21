25291b1f787ce0bd5b0e161337caef6589d63b15
Marcus79 <37101701+Marcus79@users.noreply.github.com>
2021-08-21 09:59:05 +0200

    Switch to production

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from datetime import datetime
 app = Flask(__name__)
 
 
-ENV = 'dev'
+ENV = 'prod'
 if ENV == 'dev':
     app.debug = True
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:sa@localhost/yoga'
